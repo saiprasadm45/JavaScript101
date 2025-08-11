be615faa8f4b245331efdaed169fd0f0d38be804
saiprasadm45 <saiprasadsm45@gmail.com>
2025-08-11 21:47:22 +0530

    practiced conversion on boolean

---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,47 @@
+/* conversion */
+
+let isMarried = "sundar";
+console.log(isMarried)
+let nextans = Boolean(isMarried)
+console.log(nextans)
+console.log(typeof nextans)
+
+// for boolean -> 0, " " = false, 1,2.. "hitesh" = true
+
+let temp2 = "22";
+console.log(temp2)
+console.log(typeof temp2)
+let check1 = Number(temp2)
+console.log(check1)
+console.log(typeof check1)
+
+// for numbers -> numbers are numbers eg "22", but if string(eg. "2abv") then its nan its type number, if empty string then its 0.
+// for string -> its directly convert and log as it is but the type change to string.
+let temp = true;
+console.log(temp)
+console.log(typeof temp)
+let check = String(temp)
+console.log(check)
+console.log(typeof check)
+
+
+console.log("okey")
+console.log(null == 0) // false 
+console.log(null > 0)   //false (0 > 0)
+console.log(null >= 0)  //true (0 >= 0)
+// comparision operators andx equality check operators work diffrently, comparisionns convert null to a number, treating as 0, thats why 
+
+console.log(undefined == 0) //false
+console.log(undefined > 0)  //false
+console.log(undefined >= 0) //false
+console.log(undefined <= 0) //false
+
+console.log(true > 0)
+console.log(true >= 0)
+console.log(false > 0)
+console.log(true <= 0)
+console.log(true >= "sarj")
+console.log(true >= 12)
+console.log(false >= '')
+console.log("true" == true)
+
