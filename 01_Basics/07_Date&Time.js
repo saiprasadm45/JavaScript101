26d9229f5f548a862ef088ca092c5c1c7ae522fe
saiprasadm45 <saiprasadsm45@gmail.com>
2025-08-14 21:47:53 +0530

    explore date object & custom dates

--- 01_Basics/07_Date&Time.js
+++ 01_Basics/07_Date&Time.js
@@ -2,4 +2,6 @@ let mydate = new Date()
 console.log(mydate.toDateString);//
 
 let myCreatedDate = new Date(2025,8,25);
-console.log(myCreatedDate.toLocaleString())
+console.log(myCreatedDate.toLocaleString())//this will create a custom date.
+// month start from 0 i.e 0=january ,
+// day start from 1 i.e 1= monday
