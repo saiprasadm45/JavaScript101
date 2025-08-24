d8aa264b4c69865d89ad63c8bf957b461b7111c7
saiprasadm45 <saiprasadsm45@gmail.com>
2025-08-24 23:21:55 +0530

    for loops w/ break & continue

--- 05_iterations/for.js
+++ 05_iterations/for.js
@@ -13,10 +13,10 @@ for(let i = 0; i<=10; i++){
 }
 
 
-// for(let i = 1; i<=20; i++){
-//      if(i % 2 ==0){
-//         continue; //breaks the flow
-//     }
-//     console.log(i)
+for(let i = 1; i<=20; i++){
+     if(i % 2 ==0){
+        continue; //breaks the flow
+    }
+    console.log(i)
    
-// }
+}
