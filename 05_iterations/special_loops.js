f1e4a3b8289b68b55dbb329d9757820b7abb07f7
saiprasadm45 <saiprasadsm45@gmail.com>
2025-08-30 23:33:57 +0530

    special loops

--- 05_iterations/special_loops.js
+++ 05_iterations/special_loops.js
@@ -1,9 +1,48 @@
 const msg = "I Love India";
-console.log("hello " + msg)
+console.log("hello " + msg);
 
-for(let msgs of msg){
-    if(msgs.includes(" ")){
-        msgs.replace("Love", "hate")
-    console.log(msg)
-    }
+for (let msgs of msg) {
+  if (msgs.includes(" ")) {
+    msgs.replace("Love", "hate");
+    console.log(msg);
+  }
 }
+
+const data = {
+  user: "noobcoder",
+  level: "beginner",
+  likes: ["JS", "games", "memes"],
+};
+
+// Print all key-value pairs using for...in
+// Print all likes using for...of
+// Print index and value of likes using forEach
+
+for (const info in data) {
+  console.log(`${info} : ${data[info]}`);
+}
+for (const like of data.likes) {
+  console.log(like);
+}
+
+data.likes.forEach((val, index) => console.log(index, val));
+
+const student = {
+  name: "Alex",
+  age: 21,
+  courses: ["Math", "English", "Physics"],
+};
+
+// Print all keys and values from student
+// Print each course using for...of
+// Print each course with index using forEach
+
+for (let keys in student) {
+  console.log(`${keys} : ${student[keys]}`);
+}
+for (let course of student.courses) {
+  console.log(course);
+}
+student.courses.forEach((index, val,arr) => {
+  console.log(val, index,arr);
+});
