99dc7bd3dc89d9357cdb5f3e20c609e14eb5fed8
saiprasadm45 <saiprasadsm45@gmail.com>
2025-08-30 23:09:05 +0530

    loops in depth

---
+++ 05_iterations/special_loops.js
@@ -0,0 +1,9 @@
+const msg = "I Love India";
+console.log("hello " + msg)
+
+for(let msgs of msg){
+    if(msgs.includes(" ")){
+        msgs.replace("Love", "hate")
+    console.log(msg)
+    }
+}
---
+++ 05_iterations/while_dowhile.js
@@ -0,0 +1,59 @@
+let i = 1;
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+let myarr = ["flash", "batman", "shaktiman"];
+let iv = 0;
+while (iv < myarr.length) {
+  console.log(`value is ${myarr[iv]}`);
+  iv++;
+}
+
+let table = 1;
+do {
+  let ans = 0;
+  ans = table * table;
+  console.log(ans);
+  table++;
+} while (table <= 10);
+
+console.log("hello ji kya haal chal, kaise hai ap log");
+
+let ages = [53, 46, 43, 35, 23, 19, 16, 15, 9];
+
+//  for(const nums of ages){
+//     let result = (nums, ()=>{
+//         return
+//     })=> {
+
+//     }
+//  }
+
+let greetings = "hello world";
+
+for (const greet of greetings) {
+  if (greet == " ") {
+    // continue
+  }
+  console.log(`each char is: ${greet}`);
+}
+
+let user = {
+  name: "hitesh",
+  age: 27,
+  // [mySym] : "symbol",
+  email: "hitesh@google.com",
+  location: "mumbai",
+  isLoggedin: false,
+  LastLoginDays: ["saturday", "monday"],
+  paisa: true,
+//   greeting: function () {
+//     console.log("hello user, welcome back");
+//   },
+};
+
+for (let [keys] in user) {
+  console.log([keys]);
+}
