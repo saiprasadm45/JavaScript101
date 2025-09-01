84790ef01c69c766fdc679a47abda488ccf69088
saiprasadm45 <saiprasadsm45@gmail.com>
2025-09-01 22:36:51 +0530

    bgchanger project

---
+++ 07_Projects/01_bgChanger/index.js
@@ -0,0 +1,32 @@
+let button = document.getElementById("bgchange");
+
+function getrandomcolor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color = color + letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+button.addEventListener("click", () => {
+  const newcolor = getrandomcolor();
+  document.body.style.backgroundColor = newcolor;
+});
+
+// const button = document.getElementById('bgchange');
+
+// function getRandomColor() {
+//   const letters = "0123456789ABCDEF";
+//   let color = "#";
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
+
+// button.addEventListener("click", () => {
+//   const newColor = getRandomColor();
+//   document.body.style.backgroundColor = newColor;
+// });
