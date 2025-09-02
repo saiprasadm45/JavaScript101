8cbf5643eb2e4f3c5bfc6cc3db499d9ff44f678f
saiprasadm45 <saiprasadsm45@gmail.com>
2025-09-02 23:41:16 +0530

    dom basics

---
+++ 06_DOM/script.js
@@ -0,0 +1,45 @@
+// // alert("helloji")
+
+// // const getCoffy = document.getElementById("heading")
+// // console.log(getCoffy)
+
+// let boxfav = document.getElementById("box1");
+// let Hidebtn = document.getElementById("hide");
+// let Showbtn = document.getElementById("show");
+// let darkbtn = document.getElementById("dark")
+// // console.log(darkbtn.getAttributeNode('style'))
+// console.log(darkbtn.setAttribute('src', 'mybtn btn'))
+// // let lightbtn = document.getElementById('light')
+// let mybody = document.getElementById("body");
+// // let hw = document.getElementByTagName('h1')
+
+// let flag = 0;
+
+// Hidebtn.addEventListener("click", () => {
+//   boxfav.classList.add("hide");
+// });
+
+// Showbtn.addEventListener("click", () => {
+//   boxfav.classList.remove("hide");
+// });
+
+// darkbtn.addEventListener("click", () => {
+//     if(flag === 0){
+//   mybody.classList.add("darkmode");
+//         flag = 1;
+//     }else {
+//         mybody.classList.remove("darkmode")
+//         flag = 0
+//     }
+
+// });
+
+let arr = ["hello", "js", "py", "cpp"];
+
+arr.forEach((val) => {
+    // console.log(val)
+    // return val.concat("hello")
+      console.log(`hello ${val}`)
+});
+console.log(`the new : ${arr}`);
+// console.log(newarr)
