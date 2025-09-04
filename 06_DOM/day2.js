e143355f69b300a40cb1096a9fbe5ceb5b012956
saiprasadm45 <saiprasadsm45@gmail.com>
2025-09-04 23:20:29 +0530

    dom creation and delation

--- 06_DOM/day2.js
+++ 06_DOM/day2.js
@@ -1,20 +1,33 @@
 const parent = document.querySelector(".parent")
-console.log(parent)
-console.log(parent.children)
+// console.log(parent)
+// console.log(parent.children)
 
 const child = parent.children
 
 for(let i = 0; i < child.length; i++){
-    console.log(child[i].innerHTML)
+    // console.log(child[i].innerHTML)
 }
 child[2].innerHTML = "sunday"
 
 parent.children[2].style.color = 'red'
-console.log(parent.firstElementChild)
-console.log(parent.lastElementChild.innerText)
+// console.log(parent.firstElementChild)
+// console.log(parent.lastElementChild.innerText)
 let day1 = document.querySelector('.day')
-console.log(day1.style.color = 'darkBlue')
+day1.nextElementSibling.style.color = 'green'
+day1.nextElementSibling.style.fontWeight = '800'
+
 
 let mymonday = document.querySelector(".day")
-console.log(mymonday.nextElementSibling.nextElementSibling)
-console.log(parent.nextElementSibling.firstElementChild)
+mymonday.classList.add("okey")
+// console.log("hello" + mymonday.childNodes)
+
+console.log(mymonday.nextElementSibling.nextElementSibling.innerHTML)
+console.log(parent.nextElementSibling.firstElementChild.innerHTML)
+console.log(parent.childNodes)
+
+let hone = document.createElement("h1")
+hone.className = 'hone'
+hone.id = 'myid'
+hone.setAttribute("title","generateAI")
+// console.log(hone.style.color = 'red')
+// parent.appendChild(hone)
