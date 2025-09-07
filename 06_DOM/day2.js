a9c671e2709f78da2efa361e2a6aaddbc75b0284
saiprasadm45 <saiprasadsm45@gmail.com>
2025-09-07 23:39:41 +0530

    replacechild, after, insertbefore

--- 06_DOM/day2.js
+++ 06_DOM/day2.js
@@ -75,6 +75,9 @@ const removing = document.getElementById('myhead')
 removing.after(lorem)
 // lorem.remove()
 
+const learntoremove = parent2.children[2]
+learntoremove.remove()
+
 const newp = document.createElement('p')
 newp.innerHTML = 'im new para'
 // document.body.appendChild(newp)
@@ -83,5 +86,48 @@ document.body.replaceChild(newp, removing)
 const bday = document.getElementById('bornday')
 const newelment = document.createElement('h3')
 newelment.innerHTML = 'im new h3 bhidu'
-parent.appendChild(newelment)
+// parent.appendChild(newelment)
 parent.replaceChild(newelment, bday)
+
+const boldhai = document.createElement('h1')
+boldhai.innerHTML = 'im bold'
+boldhai.style.fontSize = '50px'
+// document.body.insertBefore(boldhai, document.body.firstElementChild)
+// parent.insertBefore(boldhai, parent.children[3])
+// document.body.appendChild(boldhai)
+parent.after(boldhai)
+
+const wed = document.createElement('div')
+wed.innerHTML = 'wednesday'
+parent.replaceChild(wed, parent.children[2])
+
+
+function addLanguage(...langName){
+    const ul = document.querySelector('.language')
+    langName.forEach((lang) => {
+        const li = document.createElement('li')
+        li.innerHTML = lang;
+        ul.appendChild(li)
+    })
+
+    // const li = document.createElement('li');
+    // li.innerHTML = langName
+    // document.querySelector('.language').appendChild(li)
+}
+
+addLanguage('python','ruby')
+
+function optimizelangadd (langName){
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(langName))
+    document.querySelector('.language').appendChild(li)
+}
+
+optimizelangadd('c++')
+
+let parent3 = document.querySelector('.language')
+
+const secondLang = parent3.querySelector('li:nth-child(2)')
+const newlang = document.createElement('li')
+newlang.innerHTML = 'typescript'
+parent3.replaceChild(newlang, secondLang)
