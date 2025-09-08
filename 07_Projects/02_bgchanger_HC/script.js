1cf014063fac32bf6f6de2015d1e2ca461bbf023
saiprasadm45 <saiprasadsm45@gmail.com>
2025-09-08 16:06:01 +0530

    using switch case

--- 07_Projects/02_bgchanger_HC/script.js
+++ 07_Projects/02_bgchanger_HC/script.js
@@ -8,33 +8,69 @@ buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     // console.log(e)
     // console.log(e.target);
+    const color = e.target.id;
 
-    if (e.target.id == "gray") {
-      body.style.backgroundColor = e.target.id;
-      show.innerHTML = `you selected ${e.target.id}`;
-      display.appendChild(show);
+    // using switch case
+    switch (color) {
+      case "gray":
+        body.style.backgroundColor = e.target.id;
+        show.innerHTML = `you selected ${e.target.id}`;
+        display.appendChild(show);
+        break;
+
+      case "red":
+        body.style.backgroundColor = e.target.id;
+        show.innerHTML = `you selected ${e.target.id}`;
+        display.appendChild(show);
+        break;
+
+      case "blue":
+        body.style.backgroundColor = e.target.id;
+        show.innerHTML = `you selected ${e.target.id}`;
+        display.appendChild(show);
+        break;
+
+      case "pink":
+        body.style.backgroundColor = e.target.id;
+        show.innerHTML = `you selected ${e.target.id}`;
+        display.appendChild(show);
+        break;
+
+      case "yellow":
+        body.style.backgroundColor = e.target.id;
+        show.innerHTML = `you selected ${e.target.id}`;
+        display.appendChild(show);
+        break;
     }
-    if (e.target.id == "red") {
-      body.style.backgroundColor = e.target.id;
-      show.innerHTML = `you selected ${e.target.id}`;
-      display.appendChild(show);
-    }
-    if (e.target.id == "blue") {
-      body.style.backgroundColor = e.target.id;
-      show.innerHTML = `you selected ${e.target.id}`;
-      body.style.color = "white";
-      display.appendChild(show);
-    }
-    if (e.target.id == "pink") {
-      body.style.backgroundColor = e.target.id;
-      show.innerHTML = `you selected ${e.target.id}`;
-      display.appendChild(show);
-    }
-    if (e.target.id == "yellow") {
-      body.style.backgroundColor = e.target.id;
-      show.innerHTML = `you selected ${e.target.id}`;
-      display.appendChild(show);
-      body.style.color = "black";
-    }
+
+    //using ifelse
+    
+    // if (e.target.id == "gray") {
+    //   body.style.backgroundColor = e.target.id;
+    //   show.innerHTML = `you selected ${e.target.id}`;
+    //   display.appendChild(show);
+    // }
+    // if (e.target.id == "red") {
+    //   body.style.backgroundColor = e.target.id;
+    //   show.innerHTML = `you selected ${e.target.id}`;
+    //   display.appendChild(show);
+    // }
+    // if (e.target.id == "blue") {
+    //   body.style.backgroundColor = e.target.id;
+    //   show.innerHTML = `you selected ${e.target.id}`;
+    //   body.style.color = "white";
+    //   display.appendChild(show);
+    // }
+    // if (e.target.id == "pink") {
+    //   body.style.backgroundColor = e.target.id;
+    //   show.innerHTML = `you selected ${e.target.id}`;
+    //   display.appendChild(show);
+    // }
+    // if (e.target.id == "yellow") {
+    //   body.style.backgroundColor = e.target.id;
+    //   show.innerHTML = `you selected ${e.target.id}`;
+    //   display.appendChild(show);
+    //   body.style.color = "black";
+    // }
   });
 });
