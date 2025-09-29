6f5e45c4b62c04f636c3f3cba44c11e396580304
saiprasadm45 <saiprasadsm45@gmail.com>
2025-09-29 11:12:45 +0530

    geter seter indepth

--- 11_OOP/geterseter.js
+++ 11_OOP/geterseter.js
@@ -1,19 +1,58 @@
-class car{
-    constructor(brand, model, year){
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
+// class car{
+//     constructor(brand, model, year){
+//         this._brand = brand;
+//         this._model = model;
+//         this._year = year;
+//     }
+//     get readme(){
+//         return this._brand + ' ' + this._model + ' ' + this._year;
+//     }
+//     set setme(value){
+//         this._brand = value;
+//     }
+// }
+
+
+
+// let mycar = new car('ford', 'mustang', 2023);
+
+// mycar.readme = 'BMW' //setter
+
+// console.log(mycar.readme) //getter
+
+class Car {
+    constructor(brand, model, year) {
+        this._brand = brand; // real storage
+        this._model = model;
+        this._year = year;
     }
-    get carname(){
-        return this.brand + ' ' + this.model + ' ' + this.year;
+
+    // Getter for model (to read it)
+    get model() {
+        return this._model;
     }
-    set carname(value){
-        this.brand = value;
+
+    // Setter for model (to change it)
+    set model(value) {
+        this._model = value;
+    }
+
+    // Extra: Get full info about the car
+    get info() {
+        return `${this._brand} ${this._model} ${this._year}`;
     }
 }
 
-let mycar = new car('ford', 'mustang', 2023);
+// Create a car object
+let myCar = new Car('Ford', 'Mustang', 2023);
 
-mycar.carname = 'BMW' //setter
+// Read the model using getter
+// console.log(myCar.model); // 👉 Mustang
 
-console.log(mycar.carname) //getter
+// Change the model using setter
+myCar.model = 'Explorer';
+
+// Read the full car info
+// console.log(myCar.info); // 👉 Ford Explorer 2023
+
+console.log(myCar)
