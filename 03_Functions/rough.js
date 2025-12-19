f6c05f23a888f2e7a311b21da9bcda617286446d
saiprasadm45 <saiprasadsm45@gmail.com>
2025-12-19 23:19:30 +0530

    practicing

--- 03_Functions/rough.js
+++ 03_Functions/rough.js
@@ -1,14 +1,14 @@
-// // "use strict";
-// // let vote = function(age){
-// //     if(age >= 18){
-// //         console.log('you can vote')
-// //     }
-// //     else{
-// //         console.log("you can't vote")
-// //     }
-// // }
+// "use strict";
+// let vote = function(age){
+//     if(age >= 18){
+//         console.log('you can vote')
+//     }
+//     else{
+//         console.log("you can't vote")
+//     }
+// }
 
-// // console.log(vote(22))
+// console.log(vote(22))
 
 
 
@@ -28,18 +28,22 @@
 //     console.log(this.age);
 //   },
 // };
+// max.greet();
+// max.hate();// wont work cause arrow function dont bind their own this value, they inherit this value from the enclosing lexical context at the time they are created.
 
-// const peoples = {
-//   name: "ayush",
-//   age: 17,
-//   greet() {
-//     console.log("ye bhi lelo");
-//     return `ye lo bhaisab ${this.name}`;
-//   },
-// };
+const peoples = {
+  name: "ayush",
+  age: 17,
+  greet() { //colon not required in method shorthand
+    console.log("ye bhi lelo");
+    return `ye lo bhaisab ${this.name}`; //this refers to peoples object
+  },
+};
 
-// // console.log(peoples.greet())
-// // const aalu = peoples.greet()
+// console.log(peoples.greet())
+// const aalu = peoples.greet()
+// const aalu = peoples.greet;
+// aalu();
 
 // const aalu2 = peoples.greet();
 // // const aalu3 = aalu2()
@@ -61,56 +65,57 @@
 // // console.log(`hello im ${age} year old & i work at ${work}`);
 // console.log("---------"); //in node env == In global scope,  this refers to an empty object ({}), || in Browser == In global scope, this refers to the window object..
 
-// // function checkvoters(name, age) {
-// //   this.name = name;
-// //   this.age = age;
+// function checkvoters(name, age) {
+//   this.name = name;
+//   this.age = age;
 
-// //   function verification() {
-// //     if (age >= 18) {
-// //       return `hello ${name}, congratulations you can vote!`;
-// //     } else if (age >= 58) {
-// //       return `hello ${name} sir, just stay at your home our officers come at your door`;
-// //     }else{
-// //         return `sorry, ${name} you are ${age} year old, so you can vote after ${18 - age} years.`
-// //     }
-// //   }
-
-// //   let app0 = verification();
-// //   console.log(app0)
-  
-// // }
-
-// // let person1 = new checkvoters('rajesh', 13)
-// // let person2 = new checkvoters('rahul', 24)
-// // let person3 = new checkvoters('jyotsing', 63)
-// // console.log(person1.app0)
-
-
-// class checkvoters {
-//     constructor(name, age) {
-//         this.name = name;
-//         this.age = age;
-
-//         this.storereturn = () => {
-//             if (age >= 58) {
-//                 return `hello ${this.name},sir, just stay at your home our officers come at your door. `;
-//             } else if (age >= 18) {
-//                 return `hello ${this.name} sir, congratulations you can vote!`;
-//             } else {
-//                 return `sorry, ${this.name} you are ${this.age} year old, so you can vote after ${18 - this.age} years.`;
-//             }
-//         }
-
-//         // this.result = storereturn();
-//         // this.result = result;
-
-
+//   function verification() {
+//     if (age >= 18) {
+//       return `hello ${name}, congratulations you can vote!`;
+//     } else if (age >= 58) {
+//       return `hello ${name} sir, just stay at your home our officers come at your door`;
+//     }else{
+//         return `sorry, ${name} you are ${age} year old, so you can vote after ${18 - age} years.`
 //     }
-    
-    
+//   }
+
+//   let app0 = verification();
+//   console.log(app0)
+  
 // }
 
 // let person1 = new checkvoters('rajesh', 13)
+// let person2 = new checkvoters('rahul', 24)
+// let person3 = new checkvoters('jyotsing', 63)
+// console.log(person1.app0)
+
+
+class checkvoters {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+
+        let storereturn = () => {
+            if (age >= 58) {
+                return `hello ${this.name},sir, just stay at your home our officers come at your door. `;
+            } else if (age >= 18) {
+                return `hello ${this.name} sir, congratulations you can vote!`;
+            } else {
+                return `sorry, ${this.name} you are ${this.age} year old, so you can vote after ${18 - this.age} years.`;
+            }
+        }
+
+        this.result = storereturn;
+        // this.result = result;
+
+
+    }
+    
+    
+}
+
+let person1 = new checkvoters('rajesh', 13)
+console.log(person1.result())
 // let person2 = new checkvoters('rahul', 24)
 // let person3 = new checkvoters('jyotsing', 63)
 // console.log(checkvoters)
@@ -126,14 +131,14 @@
 
 // check the array and object are empty or 
 
-let myarr = [];
-if(myarr.length === 0){
-    console.log("thisisemptyarray");
+// let myarr = [];
+// if(myarr.length === 0){
+//     console.log("thisisemptyarray");
     
-}
+// }
 
-let emptyobj = {};
-if(Object.keys(emptyobj).length === 0){
-    console.log("emmpty obj");
+// let emptyobj = {};
+// if(Object.keys(emptyobj).length === 0){
+//     console.log("emmpty obj");
     
-}
+// }
