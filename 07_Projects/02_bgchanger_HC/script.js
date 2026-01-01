46c998d1b30331e14b0449da9452119131c1e42e
saiprasadm45 <saiprasadsm45@gmail.com>
2026-01-01 15:46:33 +0530

    at proudciton level

--- 07_Projects/02_bgchanger_HC/script.js
+++ 07_Projects/02_bgchanger_HC/script.js
@@ -3,45 +3,61 @@ let body = document.getElementById("body");
 let display = document.getElementById("display");
 let show = document.createElement("h2");
 
-buttons.forEach((button) => {
-  // console.log(button)
-  button.addEventListener("click", (e) => {
-    // console.log(e)
-    // console.log(e.target);
+buttons.forEach((button)=>{
+  console.log(button);
+  
+  button.addEventListener('click', (e)=>{
     const color = e.target.id;
+    // console.log(e);
+    // console.log(e.target);
+    
+    body.style.backgroundColor = color;
+    show.textContent = `You selected ${color}`
+    display.appendChild(show)
+  })
+})
 
-    // using switch case
-    switch (color) {
-      case "gray":
-        body.style.backgroundColor = e.target.id;
-        show.innerHTML = `you selected ${e.target.id}`;
-        display.appendChild(show);
-        break;
 
-      case "red":
-        body.style.backgroundColor = e.target.id;
-        show.innerHTML = `you selected ${e.target.id}`;
-        display.appendChild(show);
-        break;
 
-      case "blue":
-        body.style.backgroundColor = e.target.id;
-        show.innerHTML = `you selected ${e.target.id}`;
-        display.appendChild(show);
-        break;
+// buttons.forEach((button) => {
+//   // console.log(button)
+//   button.addEventListener("click", (e) => {
+//     console.log(e)
+//     console.log(e.target);
+//     const color = e.target.id;
 
-      case "pink":
-        body.style.backgroundColor = e.target.id;
-        show.innerHTML = `you selected ${e.target.id}`;
-        display.appendChild(show);
-        break;
+//     // using switch case
+//     switch (color) {
+//       case "gray":
+//         body.style.backgroundColor = e.target.id;
+//         show.innerHTML = `you selected ${e.target.id}`;
+//         display.appendChild(show);
+//         break;
 
-      case "yellow":
-        body.style.backgroundColor = e.target.id;
-        show.innerHTML = `you selected ${e.target.id}`;
-        display.appendChild(show);
-        break;
-    }
+//       case "red":
+//         body.style.backgroundColor = e.target.id;
+//         show.innerHTML = `you selected ${e.target.id}`;
+//         display.appendChild(show);
+//         break;
+
+//       case "blue":
+//         body.style.backgroundColor = e.target.id;
+//         show.innerHTML = `you selected ${e.target.id}`;
+//         display.appendChild(show);
+//         break;
+
+//       case "pink":
+//         body.style.backgroundColor = e.target.id;
+//         show.innerHTML = `you selected ${e.target.id}`;
+//         display.appendChild(show);
+//         break;
+
+//       case "yellow":
+//         body.style.backgroundColor = e.target.id;
+//         show.innerHTML = `you selected ${e.target.id}`;
+//         display.appendChild(show);
+//         break;
+//     }
 
     //using ifelse
     
@@ -72,5 +88,5 @@ buttons.forEach((button) => {
     //   display.appendChild(show);
     //   body.style.color = "black";
     // }
-  });
-});
+  // });
+// });
