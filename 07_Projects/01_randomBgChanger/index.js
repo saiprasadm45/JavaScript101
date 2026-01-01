4ed1d43bd5ecc4f7a8915c7a68c71e76a0183781
saiprasadm45 <saiprasadsm45@gmail.com>
2026-01-01 15:06:11 +0530

    foundational project

--- 07_Projects/01_randomBgChanger/index.js
+++ 07_Projects/01_randomBgChanger/index.js
@@ -1,19 +1,37 @@
-let button = document.getElementById("bgchange");
+// let button = document.getElementById("bgchange");
 
-function getrandomcolor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
+// function getrandomcolor() {
+//   const letters = "0123456789ABCDEF";
+//   let color = "#";
 
-  for (let i = 0; i < 6; i++) {
-    color = color + letters[Math.floor(Math.random() * 16)];
+//   for (let i = 0; i < 6; i++) {
+//     color = color + letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
+
+// button.addEventListener("click", () => {
+//   const newcolor = getrandomcolor();
+//   document.body.style.backgroundColor = newcolor;
+// });
+
+let button = document.getElementById('bgchange')
+
+function changecolor(){
+  const letters = '0123456789ABCDEF';
+  let color = "#"
+
+  for(let i = 0; i < 6; i++){
+    color = color + letters[Math.floor(Math.random() * 16)]
   }
+
   return color;
 }
-
-button.addEventListener("click", () => {
-  const newcolor = getrandomcolor();
+button.addEventListener('click', ()=>{
+  const newcolor = changecolor()
   document.body.style.backgroundColor = newcolor;
-});
+})
+
 
 // const button = document.getElementById('bgchange');
 
